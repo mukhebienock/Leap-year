5fe02bafcf2c9951564e7100f0c9e4bc1a7c612e
Enock Mukhebi <mukhebienock@gmail.com>
2017-06-06 11:19:37 +0300

    add another condition

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,11 @@ $(document).ready(function() {
   });
 
     var leapYear = function(year) {
-      if (year % 4 === 0) {
+      if (year % 100 === 0) {
+        return false;
+      } else if (year % 4 === 0) {
         return true;
-      }
-      else {
+      } else {
         return false;
       }
     };
